7239a2e216b9459f71bb9a1d23625a5429eb7fad
Benedict <bmsteve96@gmail.com>
2020-07-10 00:51:27 +0300

    made repo private

--- src/models/db.js
+++ src/models/db.js
@@ -1,19 +1,19 @@
-const mysql = require("mysql")
+const mysql = require( "mysql" )
 
 //config file contains all myql credentials
-const dbConfig = require("../config/db.config")
+const dbConfig = require( "../config/db.config" )
 
-const db = mysql.createConnection({
-    host : process.env.DB_HOST || dbConfig.host,
+const db = mysql.createConnection( {
+    server: process.env.DB_SERVER || dbConfig.server,
     user: process.env.DB_USER || dbConfig.user,
     password: process.env.DB_PASS || dbConfig.password,
     database: process.env.DB_DATABASE || dbConfig.database,
     multipleStatements: true
-})
+} )
 
-db.connect((err) => {
-    if (err) throw err
-    console.log("Database connected...")
-})
+db.connect( ( err ) => {
+    if ( err ) throw err
+    console.log( "Database connected..." )
+} )
 
 module.exports = db
