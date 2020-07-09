589c89e3d6c6e27fbf53de65c87b02b818bd1efc
Benedict <bmsteve96@gmail.com>
2020-07-10 00:22:23 +0300

    made repo private

---
+++ src/config/db.config.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+    host : "localhost",
+    user: "root",
+    password: "thomst5v5nson",
+    database: "foodUber"
+}
+
+module.exports = dbConfig
---
+++ src/config/email.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    username: "jmahinyila@gmail.com", // generated ethereal user
+    password: "uiocfzcnzhouhrca" // generated ethereal password
+}
