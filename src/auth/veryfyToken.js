6b5db09b2734c61e775baa21fd633ca9298e8f66
Benedict <bmsteve96@gmail.com>
2020-12-21 18:26:40 +0300

    updates

--- src/auth/veryfyToken.js
+++ src/auth/veryfyToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 module.exports = (req, res, next) => {
   const token = req.header("authorization")
-  if (!token) return res.status(401).json({ msg: "Unauthorized" })
+  if (!token) return res.json({ msg: "Unauthorized" })
 
   try {
     const verified = jwt.verify(token, process.env.SECRET_TOKEN)
